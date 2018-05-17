39bf0c7b1cef7ec875f583cbbdcee764291db57c
alex <alex@slab.org>
2018-05-17 22:04:50 +0100

    Pre-crofton II

---
+++ Feedforward/Change.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Feedforward.Change where
+
+import GHC.Generics
+import qualified Data.Aeson as A
+
+{- Fires every time the content of the editor is changed. The changeObj
+is a {from, to, text, removed, origin} object containing information
+about the changes that occurred as second argument. from and to are
+the positions (in the pre-change coordinate system) where the change
+started and ended (for example, it might be {ch:0, line:18} if the
+position is at the beginning of line #19). text is an array of strings
+representing the text that replaced the changed range (split by
+line). removed is the text that used to be between from and to, which
+is overwritten by this change. This event is fired before the end of
+an operation, before the DOM updates happen.
+-}
+
+type Pos = (Int, Int)
+
+data Change = Change {cFrom :: Pos,
+                      cTo :: Pos,
+                      cText :: [String],
+                      cRemoved :: [String],
+                      cOrigin :: String,
+                      cWhen :: Double,
+                      cNewPos :: Pos
+                     }
+            | Eval {cWhen :: Double}
+            | Move {cWhen :: Double,
+                    cNewPos :: Pos
+                   }
+            | Snapshot {cName :: Maybe String,
+                        cWhen :: Double,
+                        cText :: [String]
+                       }
+            deriving (Show, Generic)
+
+instance A.ToJSON Change
+instance A.FromJSON Change
+
+type ChangeSet = [Change]
